46c5d51a5ff40fd61b10fba678caf56a17a766df
Dan Newton <danknewton@hotmail.com>
2017-09-30 13:16:37 +0100

    commit

--- java-project/src/test/java/com/lankydan/rest/person/PersonControllerTest1.java
+++ java-project/src/test/java/com/lankydan/rest/person/PersonControllerTest1.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PersonController.class)
-public class PersonControllerTest {
+public class PersonControllerTest1 {
 
   //  @Autowired
   private MockMvc mockMvc;
